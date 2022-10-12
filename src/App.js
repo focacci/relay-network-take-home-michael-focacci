0a2728ea783688ec50c6be88d0a979bcb0d12f48
Michael Focacci <michaelfocacci@gmail.com>
2022-10-12 14:11:12 -0400

    Import Section 2 and place inside app #7

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Section1 from './Section1';
+import Section2 from './Section2';
 
 function App() {
   return (
     <div className="App">
       <Section1 />
+      <Section2 />
     </div>
   );
 }
