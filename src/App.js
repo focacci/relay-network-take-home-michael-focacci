722bcf3d1ee779821f98e8338b3cc23b36947ffa
Michael Focacci <michaelfocacci@gmail.com>
2022-10-12 16:06:35 -0400

    Fetch data and print to console #9

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Section1 from './Section1';
 import Section2 from './Section2';
 
 function App() {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const url = "https://phl.carto.com/api/v2/sql?q=SELECT+*+FROM+qualified_voter_listing_2018_primary_by_ward&filename=qualified_voter_listing_2018_primary_by_ward&format=json&skipfields=cartodb_id";
+    const fetchData = async () => {
+      try {
+        const res = await fetch(url);
+        const json = await res.json();
+        console.log(json);
+        setData(json);
+      }
+      catch (error) {
+        console.log("Error fetching data:", error);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <Section1 />
