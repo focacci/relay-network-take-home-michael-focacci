3a3b3b257505bf0d28f9863c4c933d31d4dc6994
Michael Focacci <michaelfocacci@gmail.com>
2022-10-12 13:53:57 -0400

    Create function returning all Section 1 text #6

---
+++ src/Section1.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Section1() {
+    return (
+        <div>
+            <p>Top Segment of All Voters:</p>
+            <p>Percentage of All Voters that are segment:</p>
+        </div>
+    )
+}
