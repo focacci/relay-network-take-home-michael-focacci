52cef61d57bbe533f3815a1662a4566adaf897cc
Michael Focacci <michaelfocacci@gmail.com>
2022-10-12 15:54:14 -0400

    Add "thead" block to table #12

--- src/Section2.js
+++ src/Section2.js
@@ -4,21 +4,23 @@ export default function Section2() {
     return (
         <div>
             <table>
-                <tr>
-                    <th>Ward</th>
-                    <th>Republican</th>
-                    <th>Democrat</th>
-                    <th>Other Party</th>
-                    <th>Male</th>
-                    <th>Female</th>
-                    <th>Unknown Sex</th>
-                    <th>Black</th>
-                    <th>Hispanic</th>
-                    <th>White</th>
-                    <th>Other Race</th>
-                    <th>Total</th>
-                    <th>%</th>
-                </tr>
+                <thead>
+                    <tr>
+                        <th>Ward</th>
+                        <th>Republican</th>
+                        <th>Democrat</th>
+                        <th>Other Party</th>
+                        <th>Male</th>
+                        <th>Female</th>
+                        <th>Unknown Sex</th>
+                        <th>Black</th>
+                        <th>Hispanic</th>
+                        <th>White</th>
+                        <th>Other Race</th>
+                        <th>Total</th>
+                        <th>%</th>
+                    </tr>
+                </thead>
             </table>
         </div>
     )
