c1bf128a91e547a87f4173798ebfac4cc71e9b25
Michael Focacci <michaelfocacci@gmail.com>
2022-10-12 18:05:53 -0400

    State hooks for row data and segment totals #10

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Section2 from './Section2';
 
 function App() {
 
-  const [data, setData] = useState(null);
+  const [rows, setRows] = useState(null);
+  const [totals, setTotals] = useState(null);
 
   useEffect(() => {
     const url = "https://phl.carto.com/api/v2/sql?q=SELECT+*+FROM+qualified_voter_listing_2018_primary_by_ward&filename=qualified_voter_listing_2018_primary_by_ward&format=json&skipfields=cartodb_id";
