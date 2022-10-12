3c8a720774cef69c0905d1ff3b74660781304464
Michael Focacci <michaelfocacci@gmail.com>
2022-10-12 14:10:56 -0400

    Create function returning Section 2 table #7

---
+++ src/Section2.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default function Section2() {
+    return (
+        <div>
+            <table>
+                <tr>
+                    <th>Ward</th>
+                    <th>Republican</th>
+                    <th>Democrat</th>
+                    <th>Other Party</th>
+                    <th>Male</th>
+                    <th>Female</th>
+                    <th>Unknown Sex</th>
+                    <th>Black</th>
+                    <th>Hispanic</th>
+                    <th>White</th>
+                    <th>Other Race</th>
+                    <th>Total</th>
+                    <th>%</th>
+                </tr>
+            </table>
+        </div>
+    )
+}
