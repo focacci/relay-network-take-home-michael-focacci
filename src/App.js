27a805bee0d84e3e601f2e6c970a9c4c314a3536
Michael Focacci <michaelfocacci@gmail.com>
2022-10-13 15:55:59 -0400

    handleDropdownChange updates dropdownSelection hook #14

--- src/App.js
+++ src/App.js
@@ -22,6 +22,12 @@ function App() {
     { label: "White", value: "white" },
     { label: "Other Race", value: "other_race" }
   ];
+
+  const handleDropdownChange = (event) => {
+    console.log(event.target.value);
+    setDropdownSelection(event.target.value);
+  }
+
   useEffect(() => {
     const url = "https://phl.carto.com/api/v2/sql?q=SELECT+*+FROM+qualified_voter_listing_2018_primary_by_ward&filename=qualified_voter_listing_2018_primary_by_ward&format=json&skipfields=cartodb_id";
     
