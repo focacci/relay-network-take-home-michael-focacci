516a031dac72cb54ef3a44b871dcee78d278cc1d
Michael Focacci <michaelfocacci@gmail.com>
2022-10-13 19:15:26 -0400

    Final column should display percentage of selected segment, else "%" #16

--- src/Table.js
+++ src/Table.js
@@ -2,33 +2,36 @@ import React from 'react';
 import { formatAsPercent } from './utils';
 
 function TableBody({rows, segment}) {
+
     if (rows) {
         return (
-        rows.map(row =>
-            {
-                return (
-                    <tr>
-                        <td>{row["ward"]}</td>
-                        <td>{row["rep"]}</td>
-                        <td>{row["dem"]}</td>
-                        <td>{row["other_party"]}</td>
-                        <td>{row["male"]}</td>
-                        <td>{row["female"]}</td>
-                        <td>{row["unknown_sex"]}</td>
-                        <td>{row["black"]}</td>
-                        <td>{row["hispanic"]}</td>
-                        <td>{row["white"]}</td>
-                        <td>{row["other_race"]}</td>
-                        <td>{row["total"]}</td>
-                        <td>%</td>
-                    </tr> 
-                )
-            })
+            rows.map(row =>
+                {
+                    return (
+                        <tr>
+                            <td>{row["ward"]}</td>
+                            <td>{row["rep"]}</td>
+                            <td>{row["dem"]}</td>
+                            <td>{row["other_party"]}</td>
+                            <td>{row["male"]}</td>
+                            <td>{row["female"]}</td>
+                            <td>{row["unknown_sex"]}</td>
+                            <td>{row["black"]}</td>
+                            <td>{row["hispanic"]}</td>
+                            <td>{row["white"]}</td>
+                            <td>{row["other_race"]}</td>
+                            <td>{row["total"]}</td>
+                            <td>{(segment == "") ? "%" : formatAsPercent((row[segment]/row["total"])*100)}</td>
+                        </tr> 
+                    )
+                }
+            )
         )
     }
 }
 
 export default function Table({ rows, segment }) {
+
     return (
         <div>
             <table>
