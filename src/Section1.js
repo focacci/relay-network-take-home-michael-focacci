b655ffe07a25f13b77f586dd10b2f7970b67d832
Michael Focacci <michaelfocacci@gmail.com>
2022-10-13 14:09:33 -0400

    Compute the appropriate data to display in summary #11

--- src/Section1.js
+++ src/Section1.js
@@ -1,10 +1,38 @@
 import React, { useState, useEffect } from 'react'
+
+
+
 export default function Section1({ totals }) {
 
     const [totalVoters, setTotalVoters] = useState(null);
     const [topSeg, setTopSeg] = useState(null);
     const [segVoterCount, setSegVoterCount] = useState(null);
     const [segVoterPercent, setSegVoterPercent] = useState(null);
+
+    useEffect(() => {
+        /* totals is initially 'null' when rendered in parent component.
+           The following code will not run unless totals is defined */
+        if (totals) {
+
+            /* Make a copy of totals so we can retain the "total" field value after we
+               delete it from the object */
+            var copy = JSON.parse(JSON.stringify(totals));
+
+            /* Delete unneeded fields from the object. "total" is deleted so we can quickly
+               find the largest segment value */
+            delete copy["ward"];
+            delete copy["the_geom"];
+            delete copy["the_geom_webmercator"];
+            delete copy["total"];
+
+            /* Update state variables to be rendered in the HTML text */
+            setTotalVoters(totals["total"]);
+            setTopSeg(Object.keys(copy).reduce((a, b) => copy[a] > copy[b] ? a : b));
+            setSegVoterCount(totals[topSeg]);
+            setSegVoterPercent((segVoterCount/totalVoters).toFixed(2));
+        }
+    });
+
     return (
         <div>
             <p>Top Segment of All Voters:</p>
