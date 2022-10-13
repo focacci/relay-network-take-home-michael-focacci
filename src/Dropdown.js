5322e95225a9c0cc2e92e32888948d6e8d367fdc
Michael Focacci <michaelfocacci@gmail.com>
2022-10-13 15:52:53 -0400

    Create new dropdown component #14

---
+++ src/Dropdown.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Dropdown({ label, value, options, onChange }) {
+    return (
+        <label>
+            {label}
+            <select value={value} onChange={onChange}>
+                {options.map((option) => (
+                    <option value={option.value}>{option.label}</option>
+                ))}
+            </select>
+        </label>
+    );
+};
