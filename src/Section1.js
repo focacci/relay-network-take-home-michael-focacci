8b13e1c1a95d6be69c563d061819005553eea565
Michael Focacci <michaelfocacci@gmail.com>
2022-10-13 14:23:24 -0400

    segtitleMap maps data segment name to appropriate viewable name #11
    Example:
    dem -> Democrat
    female -> Female
    other_race -> Other Race

--- src/Section1.js
+++ src/Section1.js
@@ -1,6 +1,20 @@
 import React, { useState, useEffect } from 'react'
 
 
+const segTitleMap = {
+    "black": "Black",
+    "hispanic": "Hispanic",
+    "white": "White",
+    "other_race": "Other Race",
+    "male": "Male",
+    "female": "Female",
+    "unknown_sex": "Unknown Sex",
+    "dem": "Democrat",
+    "rep": "Republican",
+    "other_party": "Other Party"
+}
+
+
 function formatAsPercent(num) {
     return new Intl.NumberFormat('default', {
       style: 'percent',
