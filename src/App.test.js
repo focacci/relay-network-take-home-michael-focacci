a0678f6d95dde97226c0522406b9a88e9dbe8968
Michael Focacci <michaelfocacci@gmail.com>
2022-10-13 14:45:11 -0400

    Test prototypes steps 1-3 #13

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,31 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('Page has the proper title', () => {
+
+});
+
+test('Summary is displayed on page', () => {
+
+});
+
+test('Table is displayed on page', () => {
+
+});
+
+test('Table has the correct headers', () => {
+
+});
+
+test('Data is fetched from the API', () => {
+
+});
+
+test('Data is used to populate the table', () => {
+
+});
+
+test('Data is used to correctly update the summary', () => {
+
+});
